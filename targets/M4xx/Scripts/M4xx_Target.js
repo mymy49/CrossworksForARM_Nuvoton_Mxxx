2811336d6eb35766b8218953af1b37931eec3bfb
Yoon-Ki Hong <mymy49@nate.com>
2025-10-15 02:19:08 +0900

    Added device recognition code for M467KJHAN

--- targets/M4xx/Scripts/M4xx_Target.js
+++ targets/M4xx/Scripts/M4xx_Target.js
@@ -11,27 +11,31 @@
 
 function Reset()
 {
-  if (TargetInterface.implementation() == "j-link")
-    TargetInterface.resetAndStop(1000);
-  else
-    {
-      TargetInterface.pokeWord(0xE000EDF0, 0xA05F0003); // set C_HALT and C_DEBUGEN in DHCSR
-      TargetInterface.waitForDebugState(1000);
-      TargetInterface.pokeWord(0xE000EDFC, 0x1); // set VC_CORERESET in DEMCR
-      TargetInterface.pokeWord(0xE000ED0C, 0x05FA0004); // set SYSRESETREQ in AIRCR
-      TargetInterface.waitForDebugState(1000);
-    }
+	if (TargetInterface.implementation() == "j-link")
+		TargetInterface.resetAndStop(1000);
+	else
+	{
+		TargetInterface.pokeWord(0xE000EDF0, 0xA05F0003); // set C_HALT and C_DEBUGEN in DHCSR
+		TargetInterface.waitForDebugState(1000);
+		TargetInterface.pokeWord(0xE000EDFC, 0x1); // set VC_CORERESET in DEMCR
+		TargetInterface.pokeWord(0xE000ED0C, 0x05FA0004); // set SYSRESETREQ in AIRCR
+		TargetInterface.waitForDebugState(1000);
+	}
 }
 
 function GetPartName()
 {
-  var id = TargetInterface.peekWord(0x40000000);
-  var n = id & 0xFFFFF;
-  if (n == 0x43312)
-    return "M433SE8AE";
-  else if (n == 0x43302)
-    return "M433LE8AE";
-  return "";
+	var id = TargetInterface.peekWord(0x40000000);
+	var n = id & 0xFFFFF;
+
+	if (n == 0x43312)
+		return "M433SE8AE";
+	else if (n == 0x43302)
+		return "M433LE8AE";
+	else if (n == 0x46F41)
+		return "M467KJHAN";
+	else
+		return "";
 }
 
 function MatchPartName(name)
